30d97006bc9fd0dcce6129ad8b0b8b42bfb287bd
kiprono-tech <kiprono.tech@gmail.com>
2025-10-15 21:31:04 +0300

    Updated src

---
+++ src/models/task.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+// Define a User model
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+})
+
+module.exports = Task
