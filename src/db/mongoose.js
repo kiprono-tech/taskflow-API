611a0a9cfdb48e8b1ae7004fdc674a25aeefaf66
kiprono-tech <kiprono.tech@gmail.com>
2025-10-15 18:22:26 +0300

    update

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,37 +1,58 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
 
 // Define a User model
-const Task = mongoose.model('Task', {
-    description: {
-        type: String
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim:true
     },
-    completed: {
-        type: Boolean
+    email: {
+        type: String,
+        require: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        },
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
     }
 })
 
 // Create and save a user
-const task = new Task({
-    description: 'Learn the Mongoose library',
-    completed: false
+const me = new User({
+    name: 'wailer',
+    email: 'wailer@gmail.com',
+    age: 24
 })
 
-task.save().then(() => {
-    console.log(task)
-}).catch(() => {
+me.save().then(() => {
+    console.log(me)
+}).catch((error) => {
     console.log('Error!', error)
 })
 
-// // Create and save a user
-// const me = new User({
-//     name: 'John',
-//     age: 24
+// // Create and save a task
+// const task = new Task({
+//     description: 'Learn the Mongoose library',
+//     completed: false
 // })
 
-// me.save().then(() => {
-//     console.log(me)
+// task.save().then(() => {
+//     console.log(task)
 // }).catch(() => {
 //     console.log('Error!', error)
 // })
