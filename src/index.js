92839223a41dca47701b3a55527a38cf6fb165fa
kiprono-tech <kiprono.tech@gmail.com>
2025-10-16 18:54:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,6 +20,28 @@ app.post('/users', (req, res) => {
         });
 });
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.status(200).send(users)
+    }).catch((error) => {
+        res.status(500).send(error);
+    })
+});
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+
+    User.findById(_id).then((users) => {
+        if (!users) {
+            return res.status(404).send()
+        }
+
+        res.status(200).send(users)
+    }).catch((error) => {
+        res.status(500).send(error);
+    })
+});
+
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body);
 
@@ -30,6 +52,28 @@ app.post('/tasks', (req, res) => {
         .catch((error) => {
             res.status(400).send(error);
         });
+});
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((task) => {
+        res.status(200).send(task)
+    }).catch((error) => {
+        res.status(500).send(error);
+    })
+});
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((tasks) => {
+        if (!tasks) {
+            return res.status(404).send()
+        }
+
+        res.status(200).send(tasks)
+    }).catch((error) => {
+        res.status(500).send(error);
+    })
 });
 
 app.listen(port, () => {
