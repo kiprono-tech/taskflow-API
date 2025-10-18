46bde8b989646a008f4a3e0d3859cba033e0e9be
kiprono-tech <kiprono.tech@gmail.com>
2025-10-18 10:55:22 +0300

    Add .gitignore and Refactor auth middleware and update user routes for improved authentication flow

--- src/routers/user.js
+++ src/routers/user.js
@@ -21,9 +21,36 @@ router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
         const token = await user.generateAuthToken()
-        res.status(200).send({ user, token})
+        res.status(200).send({ user, token })
     } catch (error) {
         res.status(400).send(error)
+    }
+});
+
+router.post('/users/logout', auth, async (req, res) => {
+    const user = new User(req.body);
+
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token != req.token
+        })
+        await req.user.save()
+
+        res.status(200).send()
+    } catch (error) {
+        res.status(500).send(error)
+    }
+});
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    const user = new User(req.body);
+
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.status(200).send()
+    } catch (error) {
+        res.status(500).send(error)
     }
 });
 
